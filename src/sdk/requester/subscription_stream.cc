ff02e9172c613246b411ef423795ea42f03b3b13
rinick <rinick@gmail.com>
2017-07-15 17:58:05 -0700

    optimize cmake for vcpkg

---
+++ src/sdk/requester/subscription_stream.cc
@@ -0,0 +1,1 @@
+#include "subscription_stream.h"
---
+++ src/sdk/requester/subscription_stream.h
@@ -0,0 +1,11 @@
+#ifndef DSA_SDK_SUBSCRIPTION_STREAM_H_
+#define DSA_SDK_SUBSCRIPTION_STREAM_H_
+
+#include "dsax.h";
+
+namespace dsa {
+
+
+}  // namespace dsa
+
+#endif  // DSA_SDK_SUBSCRIPTION_STREAM_H_
