4a473c5c1b2e1fa47ebae619aa4e1722f54135ef
akawamura <akawamura@example.com>
2017-07-18 20:32:57 -0400

    re-organize tests

---
+++ test/sdk/util/enable_shared-test.cc
@@ -0,0 +1,21 @@
+//#include <iostream>
+
+#include "dsa/util.h"
+#include "gtest/gtest.h"
+
+class RefCheck : public dsa::EnableShared<RefCheck> {
+ public:
+  static int count;
+  RefCheck(int a) { ++count; }
+  ~RefCheck() { --count; }
+};
+int RefCheck::count = 0;
+
+TEST(EnableSharedTest, RefCheck) {
+  std::shared_ptr<RefCheck> ptr = dsa::make_shared<RefCheck>(1);
+  EXPECT_EQ(RefCheck::count, 1);
+  ptr->destory();
+  EXPECT_EQ(RefCheck::count, 1);
+  ptr.reset();
+  EXPECT_EQ(RefCheck::count, 0);
+}
