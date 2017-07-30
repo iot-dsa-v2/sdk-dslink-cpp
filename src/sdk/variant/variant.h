c66d1e29f37b69415c13296c994bd8b481552937
Ben Richards <blrichards@me.com>
2017-07-30 10:48:18 -0700

    FindMsgpack support

--- src/sdk/variant/variant.h
+++ src/sdk/variant/variant.h
@@ -19,33 +19,27 @@ class Variant : public BaseVariant {
   enum : int { Null = 0, Double, Int, Bool, String, Map, Array, Binary };
 
  public:
-  Variant(int64_t v);
-  Variant(int32_t v);
-  Variant(int16_t v);
-
-  Variant(double v);
-
-  Variant(bool v);
-
-  Variant(const std::string& v);
-  Variant(const char* v);
-  Variant(const char* v, size_t size);
-
-  Variant(const std::vector<uint8_t>& v);
-  Variant(const uint8_t* data, size_t size);
-
+  explicit Variant(int64_t v);
+  explicit Variant(int32_t v);
+  explicit Variant(int16_t v);
+  explicit Variant(double v);
+  explicit Variant(bool v);
+  explicit Variant(const std::string &v);
+  explicit Variant(const char *v);
+  explicit Variant(const char *v, size_t size);
+  explicit Variant(const std::vector<uint8_t> &v);
+  explicit Variant(const uint8_t *data, size_t size);
 
   Variant();
 
  protected:
-  Variant(const std::shared_ptr<std::map<std::string, Variant>>& v);
-  Variant(const std::shared_ptr<std::vector<Variant>>& v);
+  explicit Variant(const std::shared_ptr<std::map<std::string, Variant>> &v);
+  Variant(const std::shared_ptr<std::vector<Variant>> &v);
 
  public:
   static Variant new_map();
   static Variant new_array();
 
- public:
   bool is_double() const { return which() == Double; }
   bool is_int() const { return which() == Int; }
   bool is_bool() const { return which() == Bool; }
@@ -58,16 +52,16 @@ class Variant : public BaseVariant {
   double get_double() const { return boost::get<double>(*this); }
   int64_t get_int() const { return boost::get<int64_t>(*this); }
   bool get_bool() const { return boost::get<bool>(*this); }
-  const std::string& get_string() const {
+  const std::string &get_string() const {
     return *boost::get<std::shared_ptr<std::string>>(*this);
   }
-  std::map<std::string, Variant>& get_map() const {
+  std::map<std::string, Variant> &get_map() const {
     return *boost::get<std::shared_ptr<std::map<std::string, Variant>>>(*this);
   }
-  std::vector<Variant>& get_array() const {
+  std::vector<Variant> &get_array() const {
     return *boost::get<std::shared_ptr<std::vector<Variant>>>(*this);
   }
-  const std::vector<uint8_t>& get_binary() const {
+  const std::vector<uint8_t> &get_binary() const {
     return *boost::get<std::shared_ptr<const std::vector<uint8_t>>>(*this);
   }
 
