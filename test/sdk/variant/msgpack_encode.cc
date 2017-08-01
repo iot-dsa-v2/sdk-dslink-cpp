1e6a14cc69ec200d181a6bbc3ded037dbef612c6
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-08-01 14:38:20 -0400

    Add unittests

--- test/sdk/variant/msgpack_encode.cc
+++ test/sdk/variant/msgpack_encode.cc
@@ -4,19 +4,36 @@
 #include <msgpack.h>
 
 using namespace dsa;
+typedef std::unique_ptr<Variant> VariantPtr;
 
 TEST(VariantTest, MsgpackEncoding) {
-  Variant v(123);
 
-  std::vector<uint8_t> *encoded_msg = v.to_msgpack();
-  uint8_t encoded_buf[1024];
-  std::copy(encoded_msg->begin(), encoded_msg->end(), encoded_buf);
-  size_t encoded_buf_size = encoded_msg->size();
+  {
+    VariantPtr v = VariantPtr(Variant::create(1.23));
 
-  Variant *v_dash = Variant::from_msgpack(reinterpret_cast<const uint8_t *>(encoded_buf), encoded_buf_size);
+    std::vector<uint8_t> *encoded_msg = v->to_msgpack();
+    uint8_t encoded_buf[1024];
+    std::copy(encoded_msg->begin(), encoded_msg->end(), encoded_buf);
+    size_t encoded_buf_size = encoded_msg->size();
 
-  EXPECT_TRUE(v_dash->is_int());
-  EXPECT_EQ(123, v_dash->get_int());
+    Variant *v_dash = Variant::from_msgpack(reinterpret_cast<const uint8_t *>(encoded_buf), encoded_buf_size);
+
+    EXPECT_TRUE(v_dash->is_double());
+    EXPECT_EQ(1.23, v_dash->get_double());
+  }
+  {
+    VariantPtr v = VariantPtr(Variant::create(123));
+
+    std::vector<uint8_t> *encoded_msg = v->to_msgpack();
+    uint8_t encoded_buf[1024];
+    std::copy(encoded_msg->begin(), encoded_msg->end(), encoded_buf);
+    size_t encoded_buf_size = encoded_msg->size();
+
+    Variant *v_dash = Variant::from_msgpack(reinterpret_cast<const uint8_t *>(encoded_buf), encoded_buf_size);
+
+    EXPECT_TRUE(v_dash->is_int());
+    EXPECT_EQ(123, v_dash->get_int());
+  }
 }
 
 
