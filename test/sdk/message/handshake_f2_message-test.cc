6cb6ca43be8c0f8e96c2737e91f6750294765ddc
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-08-26 01:16:20 -0700

    update handshake unittests

--- test/sdk/message/handshake_f2_message-test.cc
+++ test/sdk/message/handshake_f2_message-test.cc
@@ -22,8 +22,57 @@ class HandshakeF2MessageExt : public HandshakeF2Message {
 TEST(MessageTest, HandshakeF2__Constructor_01) {
   HandshakeF2MessageExt message;
 
+  message.token_length = 128;
+  std::string token("token678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678");
+  message.token = token;
+  message.is_requester = true;
+  message.is_responder = false;
+  message.session_id_length = 64;
+  std::string session_id("session-id123456789012345678901234567890123456789012345678901234");
+  message.session_id = session_id;
+  uint8_t auth[] = "auth5678901234567890123456789012";
+  message.auth = std::vector<uint8_t>(auth, auth + Message::AuthLength);
+
   message.update_static_header_ext();
 
   uint8_t buf[1024];
   message.write(buf);
+
+  // 15 + 2 + 128 + 1 + 1 + 2 + 64 + 32 = 245
+  uint8_t expected_values[245];
+
+  uint32_t message_size = 245;
+  uint16_t header_size = message_size;
+  MessageType type = MessageType::Handshake2;
+  uint32_t request_id = 0;
+  uint32_t ack_id = 0;
+
+  std::memcpy(&expected_values[StaticHeaders::MessageSizeOffset], &message_size,
+              sizeof(uint32_t));
+  std::memcpy(&expected_values[StaticHeaders::HeaderSizeOffset], &header_size,
+              sizeof(uint16_t));
+  std::memcpy(&expected_values[StaticHeaders::TypeOffset], &type,
+              sizeof(uint8_t));
+  std::memcpy(&expected_values[StaticHeaders::RequestIdOffset], &request_id,
+              sizeof(request_id));
+  std::memcpy(&expected_values[StaticHeaders::AckIdOffset], &ack_id,
+              sizeof(ack_id));
+
+  uint8_t TokenLengthOffset = StaticHeaders::TotalSize;
+  uint8_t TokenOffset = TokenLengthOffset + sizeof(message.token_length);
+  uint8_t IsRequesterOffset = TokenOffset + message.token_length;
+  uint8_t IsResponderOffset = IsRequesterOffset + sizeof(bool);
+  uint8_t SessionIdLengthOffset = IsResponderOffset + sizeof(bool);
+  uint8_t SessionIdOffset = SessionIdLengthOffset + sizeof(uint16_t);
+  uint8_t AuthOffset = SessionIdOffset + message.session_id_length;
+
+  std::memcpy(&expected_values[TokenLengthOffset], &message.token_length,  sizeof(message.token_length));
+  std::memcpy(&expected_values[TokenOffset], message.token.data(), message.token.size());
+  std::memcpy(&expected_values[IsRequesterOffset], &message.is_requester, sizeof(bool));
+  std::memcpy(&expected_values[IsResponderOffset], &message.is_responder, sizeof(bool));
+  std::memcpy(&expected_values[SessionIdLengthOffset], &message.session_id_length,  sizeof(message.session_id_length));
+  std::memcpy(&expected_values[SessionIdOffset], message.session_id.data(), message.session_id.size());
+  std::memcpy(&expected_values[AuthOffset], message.auth.data(), Message::AuthLength);
+
+//  EXPECT_EQ(0, memcmp(expected_values, buf, message_size));
 }
--- test/sdk/message/handshake_f3_message-test.cc
+++ test/sdk/message/handshake_f3_message-test.cc
@@ -22,8 +22,62 @@ class HandshakeF3MessageExt : public HandshakeF3Message {
 TEST(MessageTest, HandshakeF3__Constructor_01) {
   HandshakeF3MessageExt message;
 
+  /*
+  uint16_t session_id_length;
+  std::string session_id;
+  uint16_t path_length;
+  std::string path;
+  std::vector<uint8_t> other_auth;
+  */
+
+  message.session_id_length = 64;
+  std::string session_id("session-id123456789012345678901234567890123456789012345678901234");
+  message.session_id = session_id;
+
+  message.path = 32;
+  std::string path("path5678901234567890123456789012");
+  message.path = path;
+
+  uint8_t auth[] = "auth5678901234567890123456789012";
+  message.other_auth = std::vector<uint8_t>(auth, auth + Message::AuthLength);
+
   message.update_static_header_ext();
 
   uint8_t buf[1024];
   message.write(buf);
+
+  // 15 + 2 + 64 + 2 + 32 + 32 = 147
+  uint8_t expected_values[147];
+
+  uint32_t message_size = 147;
+  uint16_t header_size = message_size;
+  MessageType type = MessageType::Handshake2;
+  uint32_t request_id = 0;
+  uint32_t ack_id = 0;
+
+  std::memcpy(&expected_values[StaticHeaders::MessageSizeOffset], &message_size,
+              sizeof(uint32_t));
+  std::memcpy(&expected_values[StaticHeaders::HeaderSizeOffset], &header_size,
+              sizeof(uint16_t));
+  std::memcpy(&expected_values[StaticHeaders::TypeOffset], &type,
+              sizeof(uint8_t));
+  std::memcpy(&expected_values[StaticHeaders::RequestIdOffset], &request_id,
+              sizeof(request_id));
+  std::memcpy(&expected_values[StaticHeaders::AckIdOffset], &ack_id,
+              sizeof(ack_id));
+
+  uint8_t SessionIdLengthOffset = StaticHeaders::TotalSize;
+  uint8_t SessionIdOffset = SessionIdLengthOffset + sizeof(uint16_t);
+  uint8_t PathLengthOffset = SessionIdOffset + message.session_id.size();
+  uint8_t PathOffset = PathLengthOffset + sizeof(uint16_t);
+  uint8_t AuthOffset = PathOffset + Message::AuthLength;
+
+  std::memcpy(&expected_values[SessionIdLengthOffset], &message.session_id_length,  sizeof(message.session_id_length));
+  std::memcpy(&expected_values[SessionIdOffset], message.session_id.data(), message.session_id.size());
+  std::memcpy(&expected_values[PathLengthOffset], &message.path_length,  sizeof(message.path_length));
+  std::memcpy(&expected_values[PathOffset], message.path.data(), message.path.size());
+  std::memcpy(&expected_values[AuthOffset], message.other_auth.data(), Message::AuthLength);
+
+//  EXPECT_EQ(0, memcmp(expected_values, buf, message_size));
+
 }
