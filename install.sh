92eda84c2bf18db36797f49663b3666506237bad
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-09-05 16:21:10 -0700

    msgpack is statically built into dsa-sdk library

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 if [ ! -d build ]; then
 	mkdir build
 fi
+
+if [ ! -f deps/msgpack/CMakeLists.txt ]; then
+	git clone https://github.com/msgpack/msgpack-c.git deps/msgpack
+fi
+cp deps/CMakeLists.txt.in deps/msgpack/CMakeLists.txt
+ 
 cmake -GNinja . -DBoost_USE_STATIC_LIBS=ON -DCMAKE_INSTALL_PREFIX=. -B./build
 cd build
 # make
