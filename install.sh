ec322a682b59a3853d112921302d293611fe2446
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-09-05 17:34:53 -0700

    build msgpack out of msgpack tree

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ fi
 if [ ! -f deps/msgpack/CMakeLists.txt ]; then
 	git clone https://github.com/msgpack/msgpack-c.git deps/msgpack
 fi
-cp deps/CMakeLists.txt.in deps/msgpack/CMakeLists.txt
  
 cmake -GNinja . -DBoost_USE_STATIC_LIBS=ON -DCMAKE_INSTALL_PREFIX=. -B./build
 cd build
