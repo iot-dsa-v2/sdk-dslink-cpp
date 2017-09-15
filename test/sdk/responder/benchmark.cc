11b75b9bfa1f5f67ef432b9267fa3ae573bd00c8
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-09-15 14:07:20 -0700

    add benchmark

---
+++ test/sdk/responder/benchmark.cc
@@ -0,0 +1,127 @@
+#include "dsa/message.h"
+#include "dsa/network.h"
+
+#include "../async_test.h"
+#include "../test_config.h"
+#include "gtest/gtest.h"
+
+#include "network/tcp/tcp_client.h"
+#include "network/tcp/tcp_server.h"
+
+#include <chrono>
+#include <ctime>
+
+using namespace dsa;
+
+const int SLEEP_INTERVAL = 25;
+using high_resolution_clock = std::chrono::high_resolution_clock;
+using time_point = std::chrono::high_resolution_clock::time_point;
+
+class MockNode : public NodeModel {
+ public:
+  std::unique_ptr<SubscribeOptions> first_subscribe_options;
+  std::unique_ptr<SubscribeOptions> second_subscribe_options;
+
+  explicit MockNode(LinkStrandRef strand) : NodeModel(std::move(strand)){};
+
+  void on_subscribe(const SubscribeOptions &options) override {
+    first_subscribe_options.reset(new SubscribeOptions(options));
+    if (_subscribe_callback != nullptr) {
+      set_value(Variant("hello"));
+    }
+  }
+  void on_subscribe_option_change(const SubscribeOptions &options) override {
+    second_subscribe_options.reset(new SubscribeOptions(options));
+    if (_subscribe_callback != nullptr) {
+      set_value(Variant("world"));
+    }
+  }
+};
+
+TEST(Benchmark, One) {
+  App app;
+
+  TestConfig server_config(app);
+
+  MockNode *root_node = new MockNode(server_config.strand);
+
+  server_config.get_link_config()->set_stream_acceptor(
+      make_unique_<NodeStateManager>(ref_<MockNode>(root_node)));
+
+  WrapperConfig client_config = server_config.get_client_config(app);
+
+  app.async_start(2);
+
+  //  auto tcp_server(new TcpServer(server_config));
+  auto tcp_server = make_shared_<TcpServer>(server_config);
+  tcp_server->start();
+
+  auto tcp_client = make_shared_<TcpClient>(client_config);
+  tcp_client->connect();
+
+  ASYNC_EXPECT_TRUE(500, (*client_config.strand)(),
+                    [&]() { return tcp_client->get_session().is_connected(); });
+
+  SubscribeOptions initial_options;
+  initial_options.queue_time = 0x1234;
+  initial_options.queue_size = 0x5678;
+
+  ref_<const SubscribeResponseMessage> last_response;
+
+  const uint16_t MAX_NUM_MSGS = 0xffff;
+  uint16_t idx = 0;
+  time_point start_time_point, end_time_point;
+
+  start_time_point = high_resolution_clock::now();
+  while (idx < MAX_NUM_MSGS) {
+    auto subscribe_stream = tcp_client->get_session().requester.subscribe(
+        "",
+        [&](ref_<const SubscribeResponseMessage> &&msg,
+            IncomingSubscribeStream &stream) {
+          if (idx == (MAX_NUM_MSGS - 1)) {
+            last_response = std::move(msg);
+          }
+        },
+        initial_options);
+
+    // move out of the loop?
+    if (idx == (MAX_NUM_MSGS - 1)) {
+      int waited = 0;
+      while (waited < MAX_NUM_MSGS * 100) {
+        if (last_response != nullptr) {
+          end_time_point = high_resolution_clock::now();
+          break;
+	}
+
+        boost::this_thread::sleep(
+            boost::posix_time::milliseconds(SLEEP_INTERVAL));
+        waited += SLEEP_INTERVAL;
+      }
+    }
+
+    idx++;
+  }
+
+
+
+  std::cout << high_resolution_clock::to_time_t(start_time_point)
+            << ", " << high_resolution_clock::to_time_t(end_time_point)
+            << std::endl
+            << std::chrono::duration_cast<std::chrono::milliseconds>(
+                   end_time_point - start_time_point)
+                   .count()
+            << std::endl;
+
+  Server::close_in_strand(tcp_server);
+  Client::close_in_strand(tcp_client);
+
+  app.close();
+
+  WAIT_EXPECT_TRUE(500, [&]() { return app.is_stopped(); });
+
+  if (!app.is_stopped()) {
+    app.force_stop();
+  }
+
+  app.wait();
+}
