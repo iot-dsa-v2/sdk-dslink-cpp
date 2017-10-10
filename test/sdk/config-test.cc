0fd4abced4b7ffe3a0c3fd60d6a547ed3fbdc744
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-10-10 16:59:15 -0700

    responder tests

--- test/sdk/config-test.cc
+++ test/sdk/config-test.cc
@@ -13,7 +13,7 @@
 using namespace dsa;
 
 TEST(ConfigTest, asyncSimpleSecurityManager) {
-  App app(1);
+  App app;
 
   WrapperConfig config = TestConfig(app).get_client_config(app, true);
 
@@ -22,44 +22,35 @@ TEST(ConfigTest, asyncSimpleSecurityManager) {
 
   const uint32_t NUM_CLIENT = 2;
 
-  config.strand->post([&]() {
-    std::vector<shared_ptr_<Client>> clients;
-    for (unsigned int i = 0; i < NUM_CLIENT; ++i) {
-      shared_ptr_<Client> tcp_client(new Client(config));
-      tcp_client->connect();
-      clients.push_back(std::move(tcp_client));
+  std::vector<shared_ptr_<Client>> clients;
+  for (unsigned int i = 0; i < NUM_CLIENT; ++i) {
+    shared_ptr_<Client> tcp_client(new Client(config));
+    tcp_client->connect();
+    clients.push_back(std::move(tcp_client));
+  }
+
+  ASYNC_EXPECT_TRUE(500, *config.strand, [&]() {
+    for (auto& client : clients) {
+      if (!client->get_session().is_connected()) {
+        return false;
+      }
     }
-
-    boost::posix_time::milliseconds interval(20);
-    boost::asio::deadline_timer timer(app.io_service(), interval);
-
-    int waited = 0;
-
-    std::function<void(const boost::system::error_code&)> wait_for_connected;
-    wait_for_connected = [&](const boost::system::error_code& error) {
-      ++waited;
-      if (waited > 20) {  // waited for too long
-        EXPECT_TRUE(false);
-        app.force_stop();
-      }
-      for (auto& client : clients) {
-        if (!client->get_session().is_connected()) {
-          timer.async_wait(wait_for_connected);
-          return;
-        }
-      }
-
-      Server::close_in_strand(tcp_server);
-      for (unsigned int i = 0; i < NUM_CLIENT; ++i) {
-        Client::close_in_strand(clients[i]);
-      }
-      app.close();
-
-    };
-
-    timer.async_wait(wait_for_connected);
-
+    return true;
   });
+
+  // close everything
+  Server::close_in_strand(tcp_server);
+  for (unsigned int i = 0; i < NUM_CLIENT; ++i) {
+    Client::close_in_strand(clients[i]);
+  }
+
+  app.close();
+
+  WAIT_EXPECT_TRUE(500, [&]() { return app.is_stopped(); });
+
+  if (!app.is_stopped()) {
+    app.force_stop();
+  }
 
   app.wait();
 }
--- test/sdk/responder/responder-test.cc
+++ test/sdk/responder/responder-test.cc
@@ -110,3 +110,80 @@ TEST(ResponderTest, Subscribe) {
 
   app.wait();
 }
+
+class MockStreamAcceptor : public OutgoingStreamAcceptor {
+ public:
+  ref_<OutgoingSubscribeStream> last_subscribe_stream;
+  std::unique_ptr<SubscribeOptions> last_subscribe_options;
+  void add(ref_<OutgoingSubscribeStream> &&stream) {
+    BOOST_ASSERT_MSG(last_subscribe_stream == nullptr,
+                     "receive second subscription stream, not expected");
+    last_subscribe_stream = stream;
+    stream->send_response(
+        make_ref_<SubscribeResponseMessage>(Variant("hello")));
+    stream->on_option_change([=](OutgoingSubscribeStream &stream,
+                                 const SubscribeOptions &old_option) {
+      last_subscribe_options.reset(new SubscribeOptions(stream.options()));
+    });
+  }
+  void add(ref_<OutgoingListStream> &&stream) {}
+  void add(ref_<OutgoingInvokeStream> &&stream) {}
+  void add(ref_<OutgoingSetStream> &&stream) {}
+};
+
+
+TEST(ResponderTest, receive_message) {
+  App app;
+
+  MockStreamAcceptor *mock_stream_acceptor = new MockStreamAcceptor();
+
+  TestConfig server_config(app);
+  server_config.get_link_config()->set_stream_acceptor(
+      std::unique_ptr<MockStreamAcceptor>(mock_stream_acceptor));
+
+  WrapperConfig client_config = server_config.get_client_config(app, true);
+
+  //  auto tcp_server(new TcpServer(server_config));
+  auto tcp_server = make_shared_<TcpServer>(server_config);
+  tcp_server->start();
+
+  auto tcp_client = make_shared_<Client>(client_config);
+  tcp_client->connect();
+
+  ASYNC_EXPECT_TRUE(500, *client_config.strand,
+                    [&]() { return tcp_client->get_session().is_connected(); });
+
+  SubscribeOptions initial_options;
+  initial_options.queue_time = 0x1234;
+  initial_options.queue_size = 0x5678;
+
+  SubscribeOptions update_options;
+  update_options.queue_time = 0x9876;
+  update_options.queue_size = 0x5432;
+
+  // test invalid path scenario
+  auto subscribe_stream = tcp_client->get_session().requester.subscribe(
+      "@path",
+      [&](ref_<const SubscribeResponseMessage> &&msg,
+          IncomingSubscribeStream &stream) {
+	;
+      },
+      initial_options);
+
+
+  wait_for_bool(500, [&]() -> bool { return false; });
+
+  Server::close_in_strand(tcp_server);
+  Client::close_in_strand(tcp_client);
+
+  app.close();
+
+  WAIT_EXPECT_TRUE(500, [&]() { return app.is_stopped(); });
+
+  if (!app.is_stopped()) {
+    app.force_stop();
+  }
+
+  app.wait();
+}
+
