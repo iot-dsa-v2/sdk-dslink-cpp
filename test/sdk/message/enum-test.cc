fc2c42a63931f309b1786b8e18eb31d733ba5151
Rick Zhou <rinick@gmail.com>
2017-10-13 12:33:56 -0700

    comment out enum test for now

--- test/sdk/message/enum-test.cc
+++ test/sdk/message/enum-test.cc
@@ -4,95 +4,94 @@
 using namespace dsa;
 
 TEST(EnumTest, enum) {
-  return;
-  {
-    std::stringstream ss;
-    ss << MessageType::CLOSE;
-
-    EXPECT_EQ("CLOSE", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::SUBSCRIBE_REQUEST;
-
-    EXPECT_EQ("SUBSCRIBE_REQUEST", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::LIST_REQUEST;
-
-    EXPECT_EQ("LIST_REQUEST", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::INVOKE_REQUEST;
-
-    EXPECT_EQ("INVOKE_REQUEST", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::SET_REQUEST;
-
-    EXPECT_EQ("SET_REQUEST", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::SUBSCRIBE_RESPONSE;
-
-    EXPECT_EQ("SUBSCRIBE_RESPONSE", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::LIST_RESPONSE;
-
-    EXPECT_EQ("LIST_RESPONSE", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::INVOKE_RESPONSE;
-
-    EXPECT_EQ("INVOKE_RESPONSE", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::SET_RESPONSE;
-
-    EXPECT_EQ("SET_RESPONSE", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::HANDSHAKE0;
-
-    EXPECT_EQ("HANDSHAKE0", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::HANDSHAKE1;
-
-    EXPECT_EQ("HANDSHAKE1", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::HANDSHAKE2;
-
-    EXPECT_EQ("HANDSHAKE2", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::HANDSHAKE3;
-
-    EXPECT_EQ("HANDSHAKE3", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::ACK;
-
-    EXPECT_EQ("ACK", ss.str());
-  }
-  {
-    std::stringstream ss;
-    ss << MessageType::INVALID;
-
-    EXPECT_EQ("INVALID", ss.str());
-  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::CLOSE;
+//
+//    EXPECT_EQ("CLOSE", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::SUBSCRIBE_REQUEST;
+//
+//    EXPECT_EQ("SUBSCRIBE_REQUEST", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::LIST_REQUEST;
+//
+//    EXPECT_EQ("LIST_REQUEST", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::INVOKE_REQUEST;
+//
+//    EXPECT_EQ("INVOKE_REQUEST", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::SET_REQUEST;
+//
+//    EXPECT_EQ("SET_REQUEST", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::SUBSCRIBE_RESPONSE;
+//
+//    EXPECT_EQ("SUBSCRIBE_RESPONSE", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::LIST_RESPONSE;
+//
+//    EXPECT_EQ("LIST_RESPONSE", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::INVOKE_RESPONSE;
+//
+//    EXPECT_EQ("INVOKE_RESPONSE", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::SET_RESPONSE;
+//
+//    EXPECT_EQ("SET_RESPONSE", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::HANDSHAKE0;
+//
+//    EXPECT_EQ("HANDSHAKE0", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::HANDSHAKE1;
+//
+//    EXPECT_EQ("HANDSHAKE1", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::HANDSHAKE2;
+//
+//    EXPECT_EQ("HANDSHAKE2", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::HANDSHAKE3;
+//
+//    EXPECT_EQ("HANDSHAKE3", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::ACK;
+//
+//    EXPECT_EQ("ACK", ss.str());
+//  }
+//  {
+//    std::stringstream ss;
+//    ss << MessageType::INVALID;
+//
+//    EXPECT_EQ("INVALID", ss.str());
+//  }
 }
