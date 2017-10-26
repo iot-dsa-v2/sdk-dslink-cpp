b7fc6bc8e1ea0df87931f37fe68084ce6727ded9
Rick Zhou <rinick@gmail.com>
2017-10-25 17:01:38 -0700

    update storage interface

--- src/sdk/module/storage.h
+++ src/sdk/module/storage.h
@@ -9,6 +9,8 @@
 #include <string>
 #include <vector>
 
+#include "util/buffer.h"
+
 namespace dsa {
 
 /// key->binary storage
@@ -18,12 +20,14 @@ class QueueBucket {
                              bool key_finished)>
       ReadCallback;
 
-  virtual void array_push_back(const std::string& key, const uint8_t* data,
-                               size_t size) = 0;
-  virtual void array_remove_front(const std::string& key, size_t count) = 0;
+  virtual void push_back(const std::string& key, BytesRef&& data) = 0;
+  // when count = 0, remove all elements in the queue
+  virtual void remove_front(const std::string& key, size_t count) = 0;
 
   virtual void read_all(ReadCallback&& callback,
                         std::function<void()>&& on_done) = 0;
+
+  virtual void remove_all() = 0;
 };
 
 /// key->binary storage
@@ -32,28 +36,25 @@ class StorageBucket {
   typedef std::function<void(const std::string& key, std::vector<uint8_t> data)>
       ReadCallback;
 
-  virtual void write(const std::string& key, const uint8_t* data,
-                     size_t size) = 0;
-  virtual std::vector<uint8_t> read(const std::string& name) = 0;
+  virtual void write(const std::string& key, BytesRef&& data) = 0;
+  virtual void read(const std::string& key, ReadCallback&& callback) = 0;
   virtual void remove(const std::string& key) = 0;
 
   /// the callback might run asynchronously
   virtual void read_all(ReadCallback&& callback,
                         std::function<void()>&& on_done) = 0;
+
+  virtual void remove_all() = 0;
 };
 
 class Storage {
  public:
   /// create a bucket or find a existing bucket
   virtual StorageBucket& get_bucket(const std::string& name) = 0;
-  /// delete everything in the bucket
-  virtual void remove_bucket(const std::string& name) = 0;
 
   virtual bool queue_supported() { return false; }
   /// create a bucket or find a existing bucket
   virtual QueueBucket& get_queue_bucket(const std::string& name) = 0;
-  /// delete everything in the bucket
-  virtual void remove_queue_bucket(const std::string& name) = 0;
 };
 
 }  // namespace dsa
