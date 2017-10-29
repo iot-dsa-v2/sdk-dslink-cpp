69f2716d9ce130d410838788867f726c2decf727
Rick Zhou <rinick@gmail.com>
2017-10-29 09:18:30 -0700

    fix compile error

--- example/dslink/single_thread.cc
+++ example/dslink/single_thread.cc
@@ -3,8 +3,8 @@
 using namespace dsa;
 
 int main(int argc, const char *argv[]) {
-  App app(1);
-  ConfigLoader config(argc, argv);
+
+  ConfigLoader config(argc, argv, "mydslink", "1.0.0");
   Link link(config);
   return 0;
 }
