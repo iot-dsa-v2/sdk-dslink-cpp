74b14f8dc6c0d5d9889ab877e000099c6d363674
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-11-01 15:47:55 -0700

    update basic storage unittest

--- test/sdk/module/storage-test.cc
+++ test/sdk/module/storage-test.cc
@@ -1,22 +1,15 @@
 #include "module/default/simple_storage.h"
-#include "gtest/gtest.h"
-
 #include "core/app.h"
-
-#include <iostream>
-
+#include "../test/sdk/async_test.h"
+#include "gtest/gtest.h"
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/strand.hpp>
-
 #include <functional>
 #include <map>
 #include <string>
-
-#include "../test/sdk/async_test.h"
+#include <cstring>
 
 using namespace dsa;
-
-namespace ba = boost::asio;
 
 TEST(ModuleTest, StorageBucket) {
 
@@ -28,16 +21,6 @@ TEST(ModuleTest, StorageBucket) {
 
   StorageBucket& sb = ss.get_bucket(storage_key);
 
-  auto read_callback = [&](std::string storage_key, std::vector<uint8_t> data) {
-    std::cout << "read_callback..." << std::endl;
-    for (auto elem : data) {
-      std::cout << elem;
-    }
-    std::cout << std::endl;
-
-    return;
-  };
-
   auto on_done = []() {
     std::cout << "on_done" << std::endl;
     return;
@@ -47,30 +30,31 @@ TEST(ModuleTest, StorageBucket) {
     const char* content = {"first_item"};
     auto data = new RefCountBytes(&content[0], &content[strlen(content)]);
     sb.write(storage_key, data);
-  }
 
-  sb.read(storage_key, read_callback);
+    auto read_callback = [=] (std::string storage_key, std::vector<uint8_t> vec) {
+
+      EXPECT_EQ(0, strncmp(content, reinterpret_cast<const char *>(vec.data()), vec.size()));
+
+      return;
+    };
+    sb.read(storage_key, read_callback);
+  }
 
   {
     const char* content = {"second_item"};
     auto data = new RefCountBytes(&content[0], &content[strlen(content)]);
     sb.write(storage_key, data);
+
+    auto read_callback = [=] (std::string storage_key, std::vector<uint8_t> vec) {
+
+      EXPECT_EQ(0, strncmp(content, reinterpret_cast<const char *>(vec.data()), vec.size()));
+
+      return;
+    };
+    sb.read(storage_key, read_callback);
   }
 
-  sb.read_all(read_callback, on_done);
-
-  storage_key.assign("config1");
-
-  {
-    const char* content = {"first_item"};
-    auto data = new RefCountBytes(&content[0], &content[strlen(content)]);
-    sb.write(storage_key, data);
-  }
-
-  sb.read_all(read_callback, on_done);
-  sb.remove_all();
-
-  wait_for_bool(500, [&]() { return false; });
+  sb.remove(storage_key);
 
   app.close();
 
