300ff93537a167a7b5cad96160ca532f67d56b90
akawamura <akiyoshi.kawamura@acuitybrands.com>
2017-11-08 17:51:07 -0800

    web server implementation

--- src/sdk/web_server/web_server.cc
+++ src/sdk/web_server/web_server.cc
@@ -2,6 +2,32 @@
 
 #include "web_server.h"
 
-namespace dsa{
+#include <algorithm>
+#include <thread>
+#include <vector>
 
+namespace dsa {
+
+WebServer::WebServer(WrapperConfig& config, size_t thread_count)
+    : Server(config),
+      _thread_count(std::min(
+          std::max<std::size_t>(1, std::thread::hardware_concurrency()),
+          thread_count)) {}
+
+void WebServer::start() {
+  boost::asio::io_service ios{_thread_count};
+
+  // create and launch listener
+
+  std::vector<std::thread> v;
+  v.reserve(_thread_count - 1);
+  for (auto i = _thread_count - 1; i > 0; --i)
+    v.emplace_back([&ios] { ios.run(); });
+  ios.run();
 }
+
+void WebServer::destroy_impl() {}
+
+WebServer::~WebServer() {}
+
+}  // namespace dsa
--- src/sdk/web_server/web_server.h
+++ src/sdk/web_server/web_server.h
@@ -5,25 +5,35 @@
 #pragma once
 #endif
 
+#include "core/server.h"
+
 #include <functional>
 
 namespace dsa {
 
-class WebServer {
+class WebServer : public Server {
+ private:
+  size_t _thread_count;
+
  public:
   typedef std::function<void(WebServer&)> HttpCallback;
   typedef std::function<void(WebServer&)> WsCallback;
 
-  void add_http_handler(const string_ & path, HttpCallback&& callback);
-  void add_ws_handler(const string_ & path, WsCallback&& callback);
+  WebServer(WrapperConfig &, size_t thread_count = 4);
+  ~WebServer();
 
+  void start() override;
+  void destroy_impl() override;
+  string_ type() override { return "HTTP"; }
+
+  // HTTP server specific methods
+  void add_http_handler(const string_& path, HttpCallback&& callback);
+  void add_ws_handler(const string_& path, WsCallback&& callback);
 
   // util functions
-public:
+ public:
   void send_error(int error_code, const string_ msg = "");
-  
-
 };
-}
+}  // namespace dsa
 
 #endif  // DSA_SDK_WEB_SERVER_H_
