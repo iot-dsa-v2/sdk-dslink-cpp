e229fc9058d06ea0a830dbdbc68e139dea291fb8
Logan Gorence <loganjohngorence@gmail.com>
2017-12-29 13:14:47 -0800

    strftime replacement for MinGW.

--- src/sdk/util/date_time.cc
+++ src/sdk/util/date_time.cc
@@ -5,6 +5,11 @@
 #include <chrono>
 #include <iomanip>
 
+#ifdef __MINGW32__
+#include <cstring>
+#include <timezoneapi.h>
+#endif
+
 #if _MSC_VER
 #define localtime_r(a, b) localtime_s(b, a)
 #endif
@@ -19,22 +24,34 @@ static void update_ts(std::chrono::system_clock::time_point now) {
 
   tm localt;
   localtime_r(&t, &localt);
-
+  size_t str_size;
   char buf[32];
-  size_t str_size = std::strftime(buf, sizeof(buf), "%FT%T.000%z0", &localt);
+
+#ifdef __MINGW32__
+  TIME_ZONE_INFORMATION TimeZoneInfo;
+  GetTimeZoneInformation(&TimeZoneInfo);
+  int hourOffset = TimeZoneInfo.Bias / 60;
+  int minOffset = TimeZoneInfo.Bias % 60;
+  bool posOffset = false;
+  if (hourOffset < 0) {
+    hourOffset *= -1;
+    posOffset = true;
+  }
+  if (minOffset < 0) {
+    minOffset *= -1;
+  }
+
+  char tz[5];
+  sprintf(tz, "%s%02i:%02i", posOffset ? "+" : "-", hourOffset, minOffset);
+  sprintf(buf, "%02i-%02i-%02iT%02i:%02i:%02i.%03lli%s", localt.tm_year + 1900, localt.tm_mon, localt.tm_mday,
+          localt.tm_hour, localt.tm_min, localt.tm_sec, _last_ms.count() % 1000,
+          tz);
+  str_size = strlen(buf);
+#else
+  str_size = std::strftime(buf, sizeof(buf), "%FT%T.000%z0", &localt);
+#endif
+
   string_ str(buf, str_size);
-
-  // set milli second;
-  auto ms = _last_ms.count() % 1000;
-  str[20] = static_cast<char>((ms / 100) + '0');
-  str[21] = static_cast<char>(((ms % 100) / 10) + '0');
-  str[22] = static_cast<char>((ms % 10) + '0');
-
-  // fix format of timezone;
-  str[28] = str[27];
-  str[27] = str[26];
-  str[26] = ':';
-
   _last_ts = std::move(str);
 }
 
