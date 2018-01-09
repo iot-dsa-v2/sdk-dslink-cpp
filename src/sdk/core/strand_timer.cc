e8fc74bd03301e685dc9a1db5cb9ed2b6032bfd5
vkeles <volkan.keles@eutoenergy.com>
2018-01-09 16:59:48 +0300

    restart function added; repeat is accessable from outside to sense if the timer is running

--- src/sdk/core/strand_timer.cc
+++ src/sdk/core/strand_timer.cc
@@ -29,6 +29,11 @@ void StrandTimer::reschedule(int32_t interval_ms) {
     _timer->expires_from_now(boost::posix_time::milliseconds(interval_ms));
   }
 }
+void StrandTimer::restart(int32_t interval_ms) {
+  if (interval_ms > 0 && repeat == false) {
+    schedule(get_ref(),interval_ms);
+  }
+}
 
 void StrandTimer::schedule(ref_<StrandTimer>&& rthis, int32_t interval_ms) {
   _timer->expires_from_now(boost::posix_time::milliseconds(interval_ms));
@@ -39,7 +44,7 @@ void StrandTimer::schedule(ref_<StrandTimer>&& rthis, int32_t interval_ms) {
       canceled = (error == boost::asio::error::operation_aborted)
     ]() mutable {
       if (_callback != nullptr) {
-        bool repeat = _callback(canceled);
+        repeat = _callback(canceled);
         if (repeat && repeat_interval_ms > 0) {
           schedule(std::move(rthis), repeat_interval_ms);
         }
