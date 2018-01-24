849d471eadd23f5819c8d9f589ed57d9ff10e024
Rick Zhou <rinick@gmail.com>
2018-01-24 12:19:09 -0800

    add dslink connect test

---
+++ test/sdk/dslink/dslink-connect-test.cc
@@ -0,0 +1,78 @@
+#include "dsa/message.h"
+#include "dsa/network.h"
+#include "dsa/responder.h"
+#include "dsa/stream.h"
+
+#include <gtest/gtest.h>
+#include "../async_test.h"
+#include "../test_config.h"
+
+#include "../../broker/util/broker_runner.h"
+#include "module/default/console_logger.h"
+#include "network/tcp/tcp_server.h"
+
+using namespace dsa;
+
+TEST(DslinkTest, ConnectTest) {
+  auto app = std::make_shared<App>();
+
+  TestConfig server_strand(app);
+
+  server_strand.strand->set_responder_model(
+      make_ref_<NodeModel>(server_strand.strand->get_ref()));
+
+  auto tcp_server = server_strand.create_server();
+  tcp_server->start();
+
+  auto link = server_strand.create_dslink(true);
+
+  int step = 0;
+  link->connect(
+      [&](const shared_ptr_<Connection> connection) {
+        ++step;
+        switch (step) {
+          case 1: {
+            // connected
+            EXPECT_TRUE(connection != nullptr);
+            connection->destroy_in_strand(connection);
+            break;
+          }
+          case 2: {
+            // disconencted
+            EXPECT_TRUE(connection == nullptr);
+            break;
+          }
+          case 3: {
+            // reconnected
+            EXPECT_TRUE(connection != nullptr);
+            break;
+          }
+          default: {
+            // not expected
+            EXPECT_TRUE(false);
+          }
+        }
+      },
+      Client::FIRST_CONNECTION | Client::BROKER_INFO_CHANGE |
+          Client::EVERY_CONNECTION | Client::DISCONNECTION);
+
+  ASYNC_EXPECT_TRUE(1000, *link->strand, [&]() { return step >= 3; });
+
+  tcp_server->destroy_in_strand(tcp_server);
+  destroy_dslink_in_strand(link);
+
+  ASYNC_EXPECT_TRUE(1000, *link.get()->strand, [&]() -> bool {
+    return link->is_destroyed() && link->ref_count() == 1;
+  });
+
+  app->close();
+
+  server_strand.destroy();
+  WAIT_EXPECT_TRUE(1000, [&]() -> bool { return app->is_stopped(); });
+
+  if (!app->is_stopped()) {
+    app->force_stop();
+  }
+
+  app->wait();
+}
