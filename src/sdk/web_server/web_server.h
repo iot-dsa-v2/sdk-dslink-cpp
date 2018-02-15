814bf0219bfcd6711fbd54d4c8ce02654cc6838c
akawamura <akiyoshi.kawamura@acuitybrands.com>
2018-02-14 17:04:52 -0800

    introduce websocket class

--- src/sdk/web_server/web_server.h
+++ src/sdk/web_server/web_server.h
@@ -17,6 +17,7 @@
 #include "listener.h"
 #include "login_manager.h"
 #include "module/logger.h"
+#include "websocket.h"
 
 
 namespace dsa {
@@ -31,7 +32,7 @@ class WebServer : public std::enable_shared_from_this<WebServer> {
       boost::beast::http::request<boost::beast::http::string_body>)>
       HttpCallback;
   typedef std::function<std::shared_ptr<Connection>(
-      WebServer&, boost::asio::ip::tcp::socket&&,
+      WebServer&, Websocket,
       boost::beast::http::request<boost::beast::http::string_body>)>
       WsCallback;
 
@@ -39,8 +40,11 @@ class WebServer : public std::enable_shared_from_this<WebServer> {
   boost::asio::io_service& _io_service;
   uint16_t _port;
   std::shared_ptr<Listener> _listener;
-  shared_ptr_<LoginManager> _login_mngr;
+  uint16_t _secure_port;
+  std::shared_ptr<Listener> _secure_listener;
   boost::asio::ssl::context _context;
+
+  shared_ptr_<LoginManager> _login_mngr;
 
   // http/ws callbacks
   typedef std::pair<const string_, WsCallback&&> WsCallbackPair;
@@ -52,6 +56,7 @@ class WebServer : public std::enable_shared_from_this<WebServer> {
   ~WebServer();
 
   void listen(uint16_t port = 80);
+  void secure_listen(uint16_t port = 443);
   void start();
   void destroy();
   boost::asio::io_service& io_service() { return _io_service; }
@@ -74,8 +79,9 @@ class ErrorCallback {
   ErrorCallback(uint16_t error_code) : _error_code(error_code) {}
 
   void operator()(
-      WebServer& web_server, boost::asio::ip::tcp::socket&& socket,
+      WebServer& web_server, Websocket,
       boost::beast::http::request<boost::beast::http::string_body>&& req) {
+    // TODO - construct a proper http response
     LOG_ERROR(Logger::_(), LOG << "http error code: " << _error_code);
   }
 };
