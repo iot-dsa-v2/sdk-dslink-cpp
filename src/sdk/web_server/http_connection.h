701db5bc3801ae2cd9925e42606272068c52c101
akawamura <akiyoshi.kawamura@acuitybrands.com>
2018-02-14 23:00:45 -0800

    using websocket class in http_connection

--- src/sdk/web_server/http_connection.h
+++ src/sdk/web_server/http_connection.h
@@ -10,12 +10,14 @@
 #include <boost/beast/core/flat_buffer.hpp>
 #include <boost/beast/http.hpp>
 
+#include "websocket.h"
+
 #include <memory>
 
 namespace dsa {
 
-class WebServer;
 class Connection;
+class WebServer;
 
 // Web server side connection.
 class HttpConnection : public std::enable_shared_from_this<HttpConnection> {
@@ -23,7 +25,7 @@ class HttpConnection : public std::enable_shared_from_this<HttpConnection> {
   WebServer& _web_server;
   bool _is_secured;
   shared_ptr_<Connection> _connection;
-  boost::asio::ip::tcp::socket _socket;
+  Websocket _websocket;
   boost::asio::ssl::context _context;
   boost::beast::flat_buffer _buffer;
   boost::beast::http::request<boost::beast::http::string_body> _req;
@@ -31,7 +33,7 @@ class HttpConnection : public std::enable_shared_from_this<HttpConnection> {
  public:
   HttpConnection(WebServer& web_server, bool is_secured);
   void accept();
-  boost::asio::ip::tcp::socket& socket() { return _socket; }
+  boost::asio::ip::tcp::socket& socket() { return _websocket.socket(); }
   void destroy();
 };
 }  // namespace dsa
